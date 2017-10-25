b0dcbc284e223c49b55587504cfb4150c02e461a
Matthew Brunton <matthewagbrunton@gmail.com>
2017-10-24 21:17:13 -0400

    Edit layout and add scripts.

---
+++ Real Time Chess/Assets/Board.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+    public Dictionary<Vector2, Tile> tiles = new Dictionary<Vector2, Tile>();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Real Time Chess/Assets/BoardManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+    public Board board;
+
+    private void Awake()
+    {
+        GameObject boardObject = new GameObject("board");
+        boardObject.transform.parent = transform;
+        board = boardObject.AddComponent<Board>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
