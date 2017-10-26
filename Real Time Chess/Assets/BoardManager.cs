4a28251c37a49731656dee7717b1d57340eacbaa
Matthew Brunton <matthewagbrunton@gmail.com>
2017-10-26 19:52:55 -0400

    Spawn all pieces when starting

--- Real Time Chess/Assets/BoardManager.cs
+++ Real Time Chess/Assets/BoardManager.cs
@@ -11,6 +11,9 @@ public class BoardManager : MonoBehaviour
     // List containing all the chess pieces
     public List<GameObject> pieces;
 
+    // List containing currently active pieces
+    public List<GameObject> activePieces;
+
     // Currently selected tile. No selection provides -1.
     private int selectionX = -1;
     private int selectionY = -1;
@@ -23,9 +26,10 @@ public class BoardManager : MonoBehaviour
     //}
 
     // Use this for initialization
-    void Start () {
-		
-	}
+    void Start ()
+    {
+        spawnAllPieces();
+    }
 	
 	/// Update is called once per frame
 	void Update ()
@@ -49,7 +53,6 @@ public class BoardManager : MonoBehaviour
         {
             selectionX = (int)(hit.point.x);
             selectionY = (int)(hit.point.y);
-            Debug.Log(selectionX + ", " + selectionY);
         }
         else
         {
@@ -78,5 +81,63 @@ public class BoardManager : MonoBehaviour
             Debug.DrawLine(Vector2.right * selectionX + Vector2.up * selectionY, Vector2.right * (selectionX + 1) + Vector2.up * (selectionY + 1), Color.red);
             Debug.DrawLine(Vector2.right * selectionX + Vector2.up * (selectionY + 1), Vector2.right * (selectionX + 1) + Vector2.up * selectionY, Color.red);
         }
+    }
+
+    /// <summary>
+    /// Spawn a chess piece
+    /// </summary>
+    private void spawnPiece (int index, Vector2 position)
+    {
+        GameObject chessPiece = Instantiate(pieces[index], position, Quaternion.identity) as GameObject;
+        activePieces.Add(chessPiece);
+    }
+
+    /// <summary>
+    /// Spawn a chess piece
+    /// </summary>
+    private void spawnAllPieces()
+    {
+        activePieces = new List<GameObject>();
+
+        // Spawn White pieces
+        spawnPiece(0, getTileCenter(4, 0)); // King
+        spawnPiece(1, getTileCenter(3, 0)); // Queen
+        spawnPiece(2, getTileCenter(0, 0)); // Rook 1
+        spawnPiece(2, getTileCenter(7, 0)); // Rook 2
+        spawnPiece(3, getTileCenter(1, 0)); // Knight 1
+        spawnPiece(3, getTileCenter(6, 0)); // Knight 2
+        spawnPiece(4, getTileCenter(2, 0)); // Bishop 1
+        spawnPiece(4, getTileCenter(5, 0)); // Bishop 2
+
+        // Pawns
+        for (int i = 0; i < 8; i++)
+        {
+            spawnPiece(5, getTileCenter(i, 1));
+        }
+        
+
+        // Spawn Black pieces
+        spawnPiece(6, getTileCenter(3, 7)); // King
+        spawnPiece(7, getTileCenter(4, 7)); // Queen
+        spawnPiece(8, getTileCenter(0, 7)); // Rook 1
+        spawnPiece(8, getTileCenter(7, 7)); // Rook 2
+        spawnPiece(9, getTileCenter(1, 7)); // Knight 1
+        spawnPiece(9, getTileCenter(6, 7)); // Knight 2
+        spawnPiece(10, getTileCenter(2, 7)); // Bishop 1
+        spawnPiece(10, getTileCenter(5, 7)); // Bishop 2
+
+        // Pawns
+        for (int i = 0; i < 8; i++)
+        {
+            spawnPiece(11, getTileCenter(i, 6));
+        }
+    }
+
+    /// <summary>
+    /// Get the coordinates for the center of a tile
+    /// </summary>
+    private Vector2 getTileCenter(int x, int y)
+    {
+        return new Vector2(tileWidth * x + tileOffset, tileWidth * y + tileOffset);
     }
 }
