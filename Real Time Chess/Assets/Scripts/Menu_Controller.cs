659f1acd0c76e19c3919122e5ea401b6850c3a65
SLCTECH\nlunagariya <nikunj.lunagariya@gmail.com>
2017-12-07 15:27:51 -0500

    UI added

---
+++ Real Time Chess/Assets/Scripts/Menu_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu_Controller : MonoBehaviour 
+{
+	
+
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+
+	}
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+
+	public void Puase(Transform canvas)
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			if (canvas.GetComponentInChildren<Image>().gameObject.activeInHierarchy == false) {
+				canvas.GetComponentInChildren<Image>().gameObject.SetActive (true);
+				Debug.Log ("pause");
+			} else {
+				canvas.GetComponentInChildren<Image>().gameObject.SetActive (false);
+			}
+		}
+	}
+
+
+
+
+}
