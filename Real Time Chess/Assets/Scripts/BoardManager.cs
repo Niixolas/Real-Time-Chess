441c4527f59ca2ce861641d725df9a898b7cb97f
SLCTECH\nguibard <nguibord75@gmail.com>
2017-12-15 11:21:34 -0500

    Controller problem fixes

--- Real Time Chess/Assets/Scripts/BoardManager.cs
+++ Real Time Chess/Assets/Scripts/BoardManager.cs
@@ -40,7 +40,7 @@ public class BoardManager : MonoBehaviour
         updateSelection();
         drawChessBoard();
 
-        if (Input.GetButtonDown("Select"))
+        if (Input.GetButtonDown("Submit"))
         {
             // Detect if a mouse click is on a valid square
             if (selectionX >= 0 && selectionY >= 0)
@@ -55,7 +55,7 @@ public class BoardManager : MonoBehaviour
                 }
             }
         }
-	}
+    }
 
     /// <summary>
     /// Selects a chess piece if a piece exists in the selected square
