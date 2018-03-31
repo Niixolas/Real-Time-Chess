93007408f3e2ff05ca1e85297dfb93c247c888f3
Matthew Brunton <matthewagbrunton@gmail.com>
2018-03-31 15:15:02 -0400

    Simple main menu with controller input. Can go Game <-> Menu -> Quit.

---
+++ Real Time Chess/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetButtonDown("StartButton"))
+        {
+            SceneManager.LoadScene("_Main");
+        }
+        if (Input.GetButtonDown("SelectButton"))
+        {
+            Quit();
+        }
+    }
+
+	public void Quit()
+	{
+        Application.Quit();
+	}
+}
