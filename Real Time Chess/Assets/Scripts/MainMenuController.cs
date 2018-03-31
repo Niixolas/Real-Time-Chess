79d1f6075f7f39a94ca2034b2130b0bbb6f36a06
Matthew Brunton <matthewagbrunton@gmail.com>
2018-03-31 17:30:25 -0400

    Minor fixes and flashing "press start" text

--- Real Time Chess/Assets/Scripts/MainMenuController.cs
+++ Real Time Chess/Assets/Scripts/MainMenuController.cs
@@ -6,6 +6,13 @@ using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    public Text startText;
+
+    private void Start()
+    {
+        StartCoroutine("pressStartTextFlash");
+    }
+
     private void Update()
     {
         if (Input.GetButtonDown("StartButton"))
@@ -22,4 +29,23 @@ public class MainMenuController : MonoBehaviour
 	{
         Application.Quit();
 	}
+
+    // Flash the game over text
+    IEnumerator pressStartTextFlash()
+    {
+        while (true)
+        {
+            if (startText.enabled)
+            {
+                startText.enabled = false;
+            }
+            else
+            {
+                startText.enabled = true;
+            }
+
+            yield return new WaitForSeconds(0.75f);
+        }
+    }
+
 }
