fb0c15cf91c9e91b58f0b0e852a01b3ccb954ed5
Matthew Brunton <matthewagbrunton@gmail.com>
2018-08-24 21:53:19 -0400

    Start Menu works except for credits

--- Real Time Chess/Assets/Scripts/MainMenuController.cs
+++ Real Time Chess/Assets/Scripts/MainMenuController.cs
@@ -8,22 +8,47 @@ public class MainMenuController : MonoBehaviour
 {
     public Text startText;
 
+    public GameObject menuPawn;
+
+    private bool canMove = true;
+
     private void Start()
     {
         StartCoroutine("PressStartTextFlash");
     }
 
+    private void SetMove()
+    {
+        canMove = true;
+    }
+
     private void Update()
     {
-        if (InputController.Instance.startPressed)
+        if (InputController.Instance.startPressed || InputController.Instance.p1Pressed)
         {
-            StopAllCoroutines();
-            SceneManager.LoadScene("_Main");
+            if (menuPawn.transform.localPosition.y == -55.0f)
+            {
+                StopAllCoroutines();
+                SceneManager.LoadScene("_Main");
+            }
+            else if (menuPawn.transform.localPosition.y == -155.0f)
+            {
+                Quit();
+            }
+            
         }
 
-        if (InputController.Instance.selectPressed)
+        if (InputController.Instance.p1Move.y < 0 && menuPawn.transform.localPosition.y != -155.0f && canMove)
         {
-            Quit();
+            menuPawn.transform.localPosition = new Vector2(menuPawn.transform.localPosition.x, menuPawn.transform.localPosition.y - 50.0f);
+            canMove = false;
+            Invoke("SetMove", 0.2f);
+        }
+        else if (InputController.Instance.p1Move.y > 0 && menuPawn.transform.localPosition.y != -55.0f && canMove)
+        {
+            menuPawn.transform.localPosition = new Vector2(menuPawn.transform.localPosition.x, menuPawn.transform.localPosition.y + 50.0f);
+            canMove = false;
+            Invoke("SetMove", 0.2f);
         }
     }
 
