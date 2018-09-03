4439a6e572f243cbfe330a4ef88800bed8f384c5
Matthew Brunton <matthewagbrunton@gmail.com>
2018-09-03 16:26:47 -0400

    Some code cleanup

--- Real Time Chess/Assets/Scripts/HealthBar.cs
+++ Real Time Chess/Assets/Scripts/HealthBar.cs
@@ -5,29 +5,12 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public float CurrentHealth { get; set; }
-    public float maxHealth;
+    public float MaxHealth { get; set; }
 
     private BoardManager bm;
     
     private Image healthbar;
     private Text myText;
-
-    //GameObject newGO;
-    //Font ArialFont;
-
-    public float MaxHealth
-    {
-        get
-        {
-            //Some other code
-            return maxHealth;
-        }
-        set
-        {
-            //Some other code
-            maxHealth = value;
-        }
-    }
 
     // Use this for initialization
     void Start ()
@@ -35,26 +18,12 @@ public class HealthBar : MonoBehaviour
         bm = FindObjectOfType<BoardManager>();
 
         //resets health to full on game load
-        MaxHealth = maxHealth;        
         CurrentHealth = MaxHealth;
 
         // Get the instance of the health bar
         healthbar = GetComponent<Image>();
         healthbar.fillAmount = CalculateHealth();
         myText = GetComponentInChildren<Text>();
-
-        //newGO = new GameObject("myTextGO");
-        //myText = newGO.AddComponent<Text>();
-        //ArialFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
-        //myText.font = ArialFont;
-        //myText.material = ArialFont.material;
-        //myText.color = Color.yellow;
-        //myText.fontSize = 11;
-        //myText.fontStyle = FontStyle.Bold;
-
-        //myText.transform.SetParent(this.transform, false);
-        //myText.transform.Translate(1, -1, 0);
-
     }
 
     // Update is called once per frame
@@ -70,6 +39,7 @@ public class HealthBar : MonoBehaviour
         //deduct the damage dealt from the character's health
         CurrentHealth -= damageValue;
         healthbar.fillAmount = CalculateHealth();
+
         //if the character is out of health, die!
         if (CurrentHealth <= 0)
         {
@@ -85,10 +55,12 @@ public class HealthBar : MonoBehaviour
     void Die()
     {
         CurrentHealth = 0;
-        GetComponentInParent<ChessPiece>().HidePossibleActions();
 
+        // Disable aim boxes and collider
+        GetComponentInParent<ChessPiece>().HidePossibleActions();        
         GetComponentInParent<BoxCollider2D>().enabled = false;
 
+        // Get Particle system for death explosion
         ParticleSystem ps;
         ParticleSystem.MainModule main;
         ParticleSystem.TrailModule trails;
@@ -97,6 +69,7 @@ public class HealthBar : MonoBehaviour
         main = ps.main;
         trails = ps.trails;
 
+        // Set death explosion colors based on customizer settings
         if (GetComponentInParent<ChessPiece>().isWhite)
         {
             main.startColor = bm.customizer.whitePiecesOutlineColor;
@@ -108,99 +81,61 @@ public class HealthBar : MonoBehaviour
             trails.colorOverLifetime = bm.customizer.blackPieceGradient;
         }
 
+        // Create the death effect
         GameObject deathEffect = Instantiate(GetComponentInParent<ChessPiece>().deathExplosion, transform.parent.gameObject.transform.position, Quaternion.identity);
-
         Destroy(deathEffect, 1.0f);
 
-        if (GetComponentInParent<ChessPiece>().isWhite)
-        {
-            if (bm.blueSelectedPiece == this.GetComponentInParent<ChessPiece>())
-            {
-                float x = 0.0f;
-                Collider2D collider = null;
-                LayerMask lm = LayerMask.GetMask("BluePieces");
-
-                do
-                {
-                    x += 0.5f;
-                    collider = Physics2D.OverlapCircle(GetComponentInParent<ChessPiece>().transform.position, x, lm);
-                } while (collider == null);
-
-                bm.blueSelection = new Vector2Int(collider.GetComponentInParent<ChessPiece>().CurrentX, collider.GetComponentInParent<ChessPiece>().CurrentY);
-                bm.SelectPiece(bm.blueSelection.x, bm.blueSelection.y, 1);
-                //collider.GetComponentInParent<ChessPiece>().glow.enabled = true;
-
-                //bm.blueSelectedPiece = null;
-            }
-
-            //if (GetComponentInParent<ChessPiece>().glow.enabled)
-            //{
-            //    float x = 0.0f;
-            //    Collider2D collider = null;
-            //    LayerMask lm = LayerMask.GetMask("BluePieces");
-
-            //    do
-            //    {
-            //        x += 0.5f;
-            //        collider = Physics2D.OverlapCircle(GetComponentInParent<ChessPiece>().transform.position, x, lm);
-            //    } while (collider == null);
-
-            //    bm.blueSelection = new Vector2Int(collider.GetComponentInParent<ChessPiece>().CurrentX, collider.GetComponentInParent<ChessPiece>().CurrentY);
-            //    collider.GetComponentInParent<ChessPiece>().glow.enabled = true;
-            //}
-            
-        }
-        if (!GetComponentInParent<ChessPiece>().isWhite)
-        {
-            if (bm.redSelectedPiece == this.GetComponentInParent<ChessPiece>())
-            {
-                float x = 0.0f;
-                Collider2D collider = null;
-                LayerMask lm = LayerMask.GetMask("RedPieces");
-
-                do
-                {
-                    x += 0.5f;
-                    collider = Physics2D.OverlapCircle(GetComponentInParent<ChessPiece>().transform.position, x, lm);
-                } while (collider == null);
-
-                bm.redSelection = new Vector2Int(collider.GetComponentInParent<ChessPiece>().CurrentX, collider.GetComponentInParent<ChessPiece>().CurrentY);
-                bm.SelectPiece(bm.redSelection.x, bm.redSelection.y, 2);
-                //collider.GetComponentInParent<ChessPiece>().glow.enabled = true;
-
-                //bm.redSelectedPiece = null;
-            }
-
-            //if (GetComponentInParent<ChessPiece>().glow.enabled)
-            //{
-            //    float x = 0.0f;
-            //    Collider2D collider = null;
-            //    LayerMask lm = LayerMask.GetMask("RedPieces");
-
-            //    do
-            //    {
-            //        x += 0.5f;
-            //        collider = Physics2D.OverlapCircle(GetComponentInParent<ChessPiece>().transform.position, x, lm);
-            //    } while (collider == null);
-
-            //    bm.redSelection = new Vector2Int(collider.GetComponentInParent<ChessPiece>().CurrentX, collider.GetComponentInParent<ChessPiece>().CurrentY);
-            //    collider.GetComponentInParent<ChessPiece>().glow.enabled = true;
-            //}
-            
-        }
-
+        // If the piece was a king, set the winner for the game.
         if (transform.parent.gameObject.GetComponent<King>())
         {
             FindObjectOfType<Menu_Controller>().setWinner(!transform.parent.gameObject.GetComponent<ChessPiece>().isWhite);
         }
+        else
+        {
+            // If the piece is selected, move the selector to a nearby piece
+            if (GetComponentInParent<ChessPiece>().isWhite)
+            {
+                if (bm.blueSelectedPiece == this.GetComponentInParent<ChessPiece>())
+                {
+                    float x = 0.0f;
+                    Collider2D collider = null;
+                    LayerMask lm = LayerMask.GetMask("BluePieces");
 
-        
+                    do
+                    {
+                        x += 0.5f;
+                        collider = Physics2D.OverlapCircle(GetComponentInParent<ChessPiece>().transform.position, x, lm);
+                    } while (collider == null);
 
-        Utilities.chessBoard[GetComponentInParent<ChessPiece>().CurrentX, GetComponentInParent<ChessPiece>().CurrentY] = null;
+                    bm.blueSelection = new Vector2Int(collider.GetComponentInParent<ChessPiece>().CurrentX, collider.GetComponentInParent<ChessPiece>().CurrentY);
+                    bm.SelectPiece(bm.blueSelection.x, bm.blueSelection.y, 1);
+                }
+            }
 
-        bm.RefreshActions();
+            if (!GetComponentInParent<ChessPiece>().isWhite)
+            {
+                if (bm.redSelectedPiece == this.GetComponentInParent<ChessPiece>())
+                {
+                    float x = 0.0f;
+                    Collider2D collider = null;
+                    LayerMask lm = LayerMask.GetMask("RedPieces");
 
-        Destroy(transform.parent.gameObject);
+                    do
+                    {
+                        x += 0.5f;
+                        collider = Physics2D.OverlapCircle(GetComponentInParent<ChessPiece>().transform.position, x, lm);
+                    } while (collider == null);
+
+                    bm.redSelection = new Vector2Int(collider.GetComponentInParent<ChessPiece>().CurrentX, collider.GetComponentInParent<ChessPiece>().CurrentY);
+                    bm.SelectPiece(bm.redSelection.x, bm.redSelection.y, 2);
+                }
+            }
+
+            Utilities.chessBoard[GetComponentInParent<ChessPiece>().CurrentX, GetComponentInParent<ChessPiece>().CurrentY] = null;
+
+            bm.RefreshActions();
+
+            Destroy(transform.parent.gameObject);
+        }
     }
-
 }
